31f1e8453c469e3e5a047f8405859d8fa4074b60
pbkr-0795 <pbkr0795@gmail.com>
2023-02-14 18:17:11 +0000

    add modified files

--- Manifest/01-vpc-modules/c4-01-vpc-variables.tf
+++ Manifest/01-vpc-modules/c4-01-vpc-variables.tf
@@ -1,70 +1,76 @@
-#VPC Input Variables
+# VPC Input Variables
 
-#VPC Nmae
+# VPC Name
 variable "vpc_name" {
-  description = "VPC-Name"
-  type = string
-  default = "MY-VPC"
+  description = "VPC Name"
+  type = string 
+  default = "myvpc"
 }
 
-#VPC CIDR_BLOCK
+# VPC CIDR Block
 variable "vpc_cidr_block" {
-  description = "VPC CIDR BLOCK"
-  type = string
+  description = "VPC CIDR Block"
+  type = string 
   default = "10.0.0.0/16"
 }
 
-#VPC Availability Zones
+# VPC Availability Zones
 variable "vpc_availability_zones" {
-  description = "VPC AVAILABILITY ZONES"
-  type = string
+  description = "VPC Availability Zones"
+  type = list(string)
   default = ["us-east-1a", "us-east-1b"]
 }
 
-#VPC Pubic_Subnets
+# VPC Public Subnets
 variable "vpc_public_subnets" {
   description = "VPC Public Subnets"
-  type = string
+  type = list(string)
+  default = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+# VPC Private Subnets
+variable "vpc_private_subnets" {
+  description = "VPC Private Subnets"
+  type = list(string)
   default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-#VPC Private_subnets
-variable "vpc_private_subnets" {
-  description = "VPC PRIVATE SUBNETS"
-  type = string
-  default = ["10.0.101.0/24", "10.0.102.24"]
-}
-
-#VPC Database_subnets
+# VPC Database Subnets
 variable "vpc_database_subnets" {
-  description = "VPC database subnets"
-  type = string
+  description = "VPC Database Subnets"
+  type = list(string)
   default = ["10.0.151.0/24", "10.0.152.0/24"]
 }
 
-#Creating Vpc database subnet group
-variable "vpc_database_subnet_group" {
-  description = "VPC Database subnet group"
+# VPC Create Database Subnet Group (True / False)
+variable "vpc_create_database_subnet_group" {
+  description = "VPC Create Database Subnet Group"
   type = bool
-  default = "true"
-}
-#Creating VPC database subnet route table
-variable "vpc_database_subnet_route_table" {
-  description = "VPC Database subnet Route Table"
-  type = bool
-  default = "true"
+  default = true 
 }
 
-#VPC Enable Nat Gateway 
+# VPC Create Database Subnet Route Table (True or False)
+variable "vpc_create_database_subnet_route_table" {
+  description = "VPC Create Database Subnet Route Table"
+  type = bool
+  default = true   
+}
+
+  
+# VPC Enable NAT Gateway (True or False) 
 variable "vpc_enable_nat_gateway" {
-  description = "VPC Enable nat gateway for outbound traffic to connect internet world "
+  description = "Enable NAT Gateways for Private Subnets Outbound Communication"
   type = bool
-  default = "true"
+  default = true  
 }
 
-#VPC Single Nat Gateway
+# VPC Single NAT Gateway (True or False)
 variable "vpc_single_nat_gateway" {
-  description = "VPC Single nat gateway dpends on az's"
+  description = "Enable only single NAT Gateway in one Availability Zone to save costs during our demos"
   type = bool
-  default = "true"
+  default = true
 }
+
+
+
+
